a3029601c273e44b22cd0c7214aa1d1cc9d85565
Gobi Selva <gobi21@hotmail.com>
2014-11-30 00:16:09 +0100

    Benutzerkonto:  -> Login Fehlermeldung bei falsche Anmelde-Versuch
    Artikel
     -> Model Klasse erstellt

--- templates/customer.login.form.tmpl.php
+++ templates/customer.login.form.tmpl.php
@@ -1,12 +1,21 @@
 			<form action="" method="post" name="ss-form-login">
             	<input type="hidden" name="SSForm[<?=$FORM_ID?>][action]" value="<?=$action?>" />
+<?
+			if($login_error):
+?> 
+                <p class="ss-login_error"><?=$login_error?></p>
+<?
+			endif;
+?>
                 <p class="ss-email">
                     <label for="ss-email"><?=$label_email?></label>
-                    <input id="ss-email" name="SSForm[<?=$FORM_ID?>][<?=$fn_email?>]" type="text" class="ss-email" value="" />
+                    <input id="ss-email" name="SSForm[<?=$FORM_ID?>][<?=$fn_email?>]" type="text" 
+                    	class="ss-email required <?=$login_error?'error':''?>" value="" />
                 </p>
                 <p class="ss-password">
                     <label for="ss-password"><?=$label_password?></label>
-                    <input id="ss-password" name="SSForm[<?=$FORM_ID?>][<?=$fn_password?>]" type="text" class="ss-password" value="" />
+                    <input id="ss-password" name="SSForm[<?=$FORM_ID?>][<?=$fn_password?>]" type="text" 
+                    	class="ss-password required <?=$login_error?'error':''?>" value="" />
                 </p>
                 <p class="ss-submit">
                     <input id="ss-submit" name="SSForm[<?=$FORM_ID?>][submit]" type="submit" class="ss-submit" value="<?=$label_submit?>" />
--- templates/customer.logout.form.tmpl.php
+++ templates/customer.logout.form.tmpl.php
@@ -1,6 +1,7 @@
 			<form action="" method="post" name="ss-form-logout" class="ss-form-logout">
             	<input type="hidden" name="SSForm[<?=$FORM_ID?>][action]" value="<?=$action?>" />
                 <p class="ss-submit">
+                    <label for="ss-user"><?=$label_customer?></label>
                     <input id="ss-submit" name="SSForm[<?=$FORM_ID?>][submit]" type="submit" class="ss-submit" value="<?=$label_submit?>" />
                 </p>
 			</form>
