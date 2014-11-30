de9402669667d2e6311b6883c13bf2fb44415359
Gobi Selva <gobi21@hotmail.com>
2014-11-30 23:55:22 +0100

    Cart - add to cart, remove from cart, delete cart

--- classes/controller/SSCartController.php
+++ classes/controller/SSCartController.php
@@ -1,5 +1,17 @@
 <?php
-class SSCartController {
+#
+#
+# SSCartController
+# https://github.com/last-hero/square_shop
+#
+# (c) Gobi Selva
+# http://www.square.ch
+#
+# Diese Klasse verwaltet den ganzen Warenkorb
+#
+#
+
+class SSCartController {	
 	// Singleton --> Session Objekt
 	private $session;
 	
@@ -24,17 +36,100 @@ class SSCartController {
     }
 	
 	/*
-	* Login/Logout Funktion starten
+	* Warenkorb starten
 	*/
-	public function invoke(){		
+	public function invoke(){
+		$this->cartHandler();
 		$this->displayView();
 	}
 	
 	/*
-	* Falls User nicht angemeldet: Login-Maske anzeigen
-	* Falls User angemeldet: Logout-Maske anzeigen
+	* Warenkorb Handler: Add to Cart, Remove from Cart, Menge ändern
+	*/
+	public function cartHandler(){
+		switch($this->formPropertiesAndValues['action']){
+			case SSArticleController::ACTION_ADD_TO_CART:
+				if($this->isInputAddToCartValid()){
+					$artId = (int)$this->formPropertiesAndValues['id'];
+					$qty = (int)$this->formPropertiesAndValues['qty'];
+					$this->addToCart($artId, $qty);
+				}
+				break;
+			case 'null':
+				break;
+		}
+	}
+	
+	/*
+	* Add To Cart Inputs überprüfen
+	*/
+	public function isInputAddToCartValid(){
+		$artId = (int)$this->formPropertiesAndValues['id'];
+		$qty = (int)$this->formPropertiesAndValues['qty'];
+		$article = new SSArticle();
+		if($article->loadById($artId)){
+			return true;
+		}
+		return false;
+	}
+	
+	/*
+	* Artikel zum Warenkorb hinzufügen
+	* Dabei werden ID, Qty in Session gespeichert
+	* param int $artId: Artikel ID
+	* param int $qty: Menge
+	*/
+	public function addToCart($artId, $qty){
+		$items = $this->session->get('cartItems');
+		$updated = false;
+		for($i=0; $i<sizeof($items); $i++){
+			if((int)$items[$i]['id'] == $artId){
+				$items[$i]['qty'] = (int)$items[$i]['qty'] + $qty;
+				$updated = true;
+			}
+		}
+		if(!$updated){
+			$items[] = array('id' => $artId, 'qty' => $qty);
+		}
+		$this->session->set('cartItems', $items);
+	}
+	
+	/*
+	* Artikel vom Warenkorb löschen
+	*/
+	public function removeFromCart($artId){
+		$items = $this->session->get('cartItems');
+		for($i=0; $i<sizeof($items); $i++){
+			if((int)$items[$i]['id'] == $artId){
+				unset($items[$i]);
+			}
+		}
+		$this->session->set('cartItems', $items);
+	}
+	
+	/*
+	* Artikel zum Warenkorb hinzufügen
+	* Dabei werden ID, Qty in Session gespeichert
+	*/
+	public function clearCart(){
+		$this->session->remove('cartItems');
+	}
+	
+	/*
+	* berechnet Total + Subtotal neu
+	*/
+	public function calcTotal(){
+	}
+	
+	/*
 	*/
 	public function displayView(){
+		
+	}
+	
+	/*
+	*/
+	public function displayCart(){
 		
 	}
 }
---
+++ classes/view/SSCartVew.php
@@ -0,0 +1,44 @@
+<?php
+#
+#
+# SSCartView
+# https://github.com/last-hero/square_shop
+#
+# (c) Gobi Selva
+# http://www.square.ch
+#
+# Dies Klasse enthält alle Views für den Warenkorb
+#
+#
+
+class SSCartView {
+	// Form Array Key Name
+	const FORM_ID = 'cart';
+	
+	/**
+	* Warenkorb Anzeige
+	*   --> Full
+	*/
+	public function displayCart($params = array()){
+		$params['FORM_ID'] = self::FORM_ID;
+		try{			
+			echo SSGUI::parse(self::FORM_ID.'.complete.tmpl.php', $params);
+		}catch(SSException $e) {
+			echo $e;
+		}
+	}
+	
+	/**
+	* Warenkorb Anzeige
+	*   --> Short
+	*/
+	public function displayCartShort($params = array()){
+		$params['FORM_ID'] = self::FORM_ID;
+		try{			
+			echo SSGUI::parse(self::FORM_ID.'.short.tmpl.php', $params);
+		}catch(SSException $e) {
+			echo $e;
+		}
+	}
+}
+
