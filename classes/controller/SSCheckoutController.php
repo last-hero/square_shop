8bb130565e5b725176df2094701e0d7b8346fd90
Gobi Selva <gobi21@hotmail.com>
2014-12-18 00:49:14 +0100

    very simple mail to customer & seller after successfull checkout bugfix keine Fehler während Installation

--- classes/controller/SSCheckoutController.php
+++ classes/controller/SSCheckoutController.php
@@ -182,7 +182,7 @@ class SSCheckoutController extends SSController{
 				break;
 		}
 	}
-	public function checkoutViewHandler(){				
+	public function checkoutViewHandler(){
 		switch($this->getStep()){
 			case 1:
 				$this->displayLoginStep();
@@ -308,6 +308,41 @@ class SSCheckoutController extends SSController{
 			, 'SelectPayment' => $this->getSession('SelectPayment')
 			, 'Step' => $this->getStep()
 		);
+		
+				
+		/* --------------------------------------------------------------
+		// Todo Mail verschicken
+		// Mail an Shop-Betreier + Käufer
+		- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+		$orderId = $this->getSession('OrderId');
+		$orders_mail_to = SSHelper::getSetting('orders_mail_to');
+		$orderItem = new SSOrderItem();
+		$orderItemsArray = $orderItem->getByForeignId($orderId, SSOrder::TABLE);
+		
+		$order = new SSOrder();
+		$order->loadById($orderId);
+		$orders_mail_to_customer = $order->get('billing_email');
+		$order_no = $order->get('no');
+		
+		$output = 'Guten Tag ';
+		$output .= $order->get('billing_firstname').' '.$order->get('billing_lastname');
+		$output .= "\r\n";
+		$output .= "\r\n";
+		for($x=0; $x<count($orderItemsArray); $x++){
+			$output .= 	'(ArtNr.)'.$orderItemsArray[$x]['no'];
+			$output .= 	' | (Artikel) '.$orderItemsArray[$x]['title'];
+			$output .= 	' | (Preis) '.$orderItemsArray[$x]['price'];
+			$output .= 	' | (Menge) '.$orderItemsArray[$x]['qty'];
+			$output .= 	' | (Subtotal) '.number_format(((int)$orderItemsArray[$x]['qty']*(int)$orderItemsArray[$x]['price']), 2, '.', '');
+			$output .= "\r\n";
+		}
+		$output .= "\r\n";
+		$output .= "\r\n";
+				
+		mail($orders_mail_to,"Bestellung - ".$order_no,$output);
+		mail($orders_mail_to_customer,"Bestellung - ".$order_no,$output);
+		/* ------------------------------------------------------------ */
+		
 		$this->cartCtrl->clearCart();
 		$this->clearAll();
 	}
@@ -387,39 +422,6 @@ class SSCheckoutController extends SSController{
 					, 'PayerEmail' => $orderDbData['payer_email']
 					, 'Step' => 7
 				);
-				
-				/* --------------------------------------------------------------
-				// Mail an Shop-Betreier + Käufer
-				- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-				// Todo Mail verschicken
-				$orders_mail_to = SSHelper::getSetting('orders_mail_to');
-				$orderItem = new SSOrderItem();
-				$orderItemsArray = $orderItem->getByForeignId($orderId, SSOrder::TABLE);
-				
-				//$order = new SSOrder();
-				//$order->loadById($orderId);
-				$orders_mail_to_customer = $order->get('billing_email');
-				$order_no = $order->get('no');
-				
-				$output = 'Guten Tag ';
-				$output .= $order->get('billing_firstname').' '.$order->get('billing_lastname');
-				$output .= "\r\n";
-				$output .= "\r\n";
-				for($x=0; $x<count($orderItemsArray); $x++){
-					$output .= 	'(ArtNr.)'.$orderItemsArray[$x]['no'];
-					$output .= 	' | (Artikel) '.$orderItemsArray[$x]['title'];
-					$output .= 	' | (Preis) '.$orderItemsArray[$x]['price'];
-					$output .= 	' | (Menge) '.$orderItemsArray[$x]['qty'];
-					$output .= 	' | (Subtotal) '.number_format(((int)$orderItemsArray[$x]['qty']*(int)$orderItemsArray[$x]['price']), 2, '.', '');
-					$output .= "\r\n";
-				}
-				$output .= "\r\n";
-				$output .= "\r\n";
-						
-				mail($orders_mail_to,"Bestellung - ".$order_no,$output);
-				mail($orders_mail_to_customer,"Bestellung - ".$order_no,$output);
-				/* ------------------------------------------------------------ */
-				
 			}
 			$this->cartCtrl->clearCart();
 			$this->clearAll();
